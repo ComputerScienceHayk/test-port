2958cec091b02e9785ea2698d985d39c8ca4645a
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-04-22 19:44:57 +0400

    Updates

---
+++ precache-manifest.e7e178b183455117732242e004cbab9d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee087b37d85fb1b2551999f4f9ced28e",
+    "url": "/test-port/index.html"
+  },
+  {
+    "revision": "37bd71f358cf5a3b64a7",
+    "url": "/test-port/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "84da76f5c5e3d8566855",
+    "url": "/test-port/static/css/main.78715687.chunk.css"
+  },
+  {
+    "revision": "37bd71f358cf5a3b64a7",
+    "url": "/test-port/static/js/2.0eb9c779.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/test-port/static/js/2.0eb9c779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84da76f5c5e3d8566855",
+    "url": "/test-port/static/js/main.0b4430b8.chunk.js"
+  },
+  {
+    "revision": "4b58cb3efae80daacb44",
+    "url": "/test-port/static/js/runtime-main.276e37f8.js"
+  },
+  {
+    "revision": "8661e960462b02aadd7c841e5e729f3c",
+    "url": "/test-port/static/media/bg1.8661e960.jpg"
+  },
+  {
+    "revision": "b3f7a77b9b394b083c1268562156e563",
+    "url": "/test-port/static/media/codepen-icon.b3f7a77b.svg"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/test-port/static/media/css.15678ffe.svg"
+  },
+  {
+    "revision": "3828361c35f98181e8c8fcd2e1ccb45d",
+    "url": "/test-port/static/media/emathy.3828361c.svg"
+  },
+  {
+    "revision": "fd633753e7643eaf60db83c13b66dfab",
+    "url": "/test-port/static/media/enthus.fd633753.svg"
+  },
+  {
+    "revision": "d8b262d77d1e016d649f9308971d40ca",
+    "url": "/test-port/static/media/express.d8b262d7.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/test-port/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "523d5e810b05e55e91dcc3e93de38e28",
+    "url": "/test-port/static/media/gitlab.523d5e81.svg"
+  },
+  {
+    "revision": "b7177eee20e296aa68068abf69797080",
+    "url": "/test-port/static/media/html5.b7177eee.png"
+  },
+  {
+    "revision": "112854f0b63e6541d739de55cb230502",
+    "url": "/test-port/static/media/js.112854f0.png"
+  },
+  {
+    "revision": "658c7f3eb3cda9df901160e1b02cd868",
+    "url": "/test-port/static/media/learn.658c7f3e.svg"
+  },
+  {
+    "revision": "7595e22f50c60e9594da0bf9fed1ece8",
+    "url": "/test-port/static/media/less.7595e22f.svg"
+  },
+  {
+    "revision": "7b7e16c2ab8dd50bb0392497079e29df",
+    "url": "/test-port/static/media/linux.7b7e16c2.svg"
+  },
+  {
+    "revision": "d1b88dbb54c5fdc841d025e4efb4a7ab",
+    "url": "/test-port/static/media/me.d1b88dbb.png"
+  },
+  {
+    "revision": "79bc24a5a740bf29d4c1a4ca26fd6136",
+    "url": "/test-port/static/media/mongo.79bc24a5.png"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/test-port/static/media/node.0d8ed365.png"
+  },
+  {
+    "revision": "4c4d12dfd879a1bff5321defd24e9803",
+    "url": "/test-port/static/media/react.4c4d12df.png"
+  },
+  {
+    "revision": "8d5fb8a0b0904bdee36452e5eb303579",
+    "url": "/test-port/static/media/respons.8d5fb8a0.svg"
+  },
+  {
+    "revision": "fa68fc051e506d210e034ba2a11a5a68",
+    "url": "/test-port/static/media/solve.fa68fc05.svg"
+  },
+  {
+    "revision": "11ad2cb7bcbbe7e553851dc9b4830cfb",
+    "url": "/test-port/static/media/teamwork.11ad2cb7.svg"
+  },
+  {
+    "revision": "8b636004ef9c00b3e30e459c1931fa8a",
+    "url": "/test-port/static/media/terminal.8b636004.svg"
+  },
+  {
+    "revision": "ea4e1d80a509fa3d9a377b1a916d5989",
+    "url": "/test-port/static/media/wind.ea4e1d80.svg"
+  }
+]);
